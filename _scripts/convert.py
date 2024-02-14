c47cef43e857ad9697e8623d2f34be19a34ac996
Aleksei Dmitriev <alex.i.dmitriev@gmail.com>
2024-02-14 17:55:34 +0100

    Automate  (#2)
    * Create update-index.yml
    
    * Create convert.py
    
    * Update update-index.yml

---
+++ _scripts/convert.py
@@ -0,0 +1,29 @@
+def convert(enc):
+    buf = bytearray()
+    idx = 10
+    while idx < len(enc):
+        b = 0
+        if enc[idx] == "?":
+            break
+        elif enc[idx] == "=":
+            b = int(enc[idx+1:idx+3], 16)
+            idx = idx + 3
+        else:
+            b = ord(enc[idx])
+            idx = idx + 1
+        buf.append(b)
+    return str(buf, "utf-8")
+        
+    
+lines = list()
+with open("headers.txt", "r") as f:
+    s = f.readline()
+    while s:
+        if s[:9] == "Subject: ":
+            s2 = f.readline()
+            line = "\"" + convert(s[9:]) + "\"," + s2[47:57] + "\n"
+            lines.append(line)
+        s = f.readline()
+
+with open("links.csv", "w") as f:
+    f.writelines(reversed(lines))
